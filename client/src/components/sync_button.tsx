82b20f7795f0631758faca14a1cbec545bdd714d
Veda <veda@vedapowered.ca>
2023-12-05 18:43:52 -0700

    Making the syncing kinda work

--- client/src/components/sync_button.tsx
+++ client/src/components/sync_button.tsx
@@ -8,19 +8,27 @@ import ListItem from "@mui/joy/ListItem";
 import ListItemButton from "@mui/joy/ListItemButton";
 
 export function SyncButton() {
-  const [loadingState, setLoadingState] = React.useState<"saved" | "saving">(
-    "saved",
-  );
-  async function doSync() {
-    setLoadingState("saving");
-    const response = await fetch("/api/event/matches");
-    localStorage.setItem("matchList", await response.text());
-    setLoadingState("saved");
-  }
+	const [loadingState, setLoadingState] = React.useState<"saved" | "saving">(
+		"saved",
+	);
+	async function doSync() {
+		setLoadingState("saving");
+		const response = await fetch("/api/event/matches");
+		localStorage.setItem("matchList", await response.text());
+		const matchArray = [];
+		for (let entry = 0; entry < localStorage.length; entry++) {
+			const key: string | null = localStorage.key(entry);
+			if (key !== null && key.startsWith("match-")) {
+				matchArray.push(JSON.parse(localStorage.getItem(key)) as MatchEntryIdData);
+			}
+		}
+		await fetch("/api/match_entry/data/all", {method: "PUT", headers: {"Content-Type": "application/json"}, body: JSON.stringify(matchArray)});
+		setLoadingState("saved");
+	}
 
-  return (
-    <Button loading={loadingState === "saving"} onClick={doSync}>
-      Save Data
-    </Button>
-  );
+	return (
+		<Button loading={loadingState === "saving"} onClick={doSync}>
+			Save Data
+		</Button>
+	);
 }
