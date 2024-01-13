e421bf4c447f2c8d80796b55fe9d21a190b82788
Jason Heard <jasonpheard@101100.ca>
2024-01-13 15:56:16 -0700

    Add background load for Statbotics

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(slice_group_by)]
 
-mod api;
 mod analysis;
+mod api;
 mod config;
 mod data_validation;
 mod database;
@@ -13,6 +13,7 @@ use crate::config::ConfigManager;
 use crate::database::Database;
 use crate::server::ScoutingServer;
 use color_eyre::Result;
+use log::info;
 use simplelog::{Config, LevelFilter, SimpleLogger};
 
 #[tokio::main]
@@ -21,5 +22,7 @@ async fn main() -> Result<()> {
 	SimpleLogger::init(LevelFilter::Info, Config::default())?;
 
 	let server = ScoutingServer::new(ConfigManager::new()?, Database::open("data")?)?;
-	server.serve("0.0.0.0:4421").await
+	let address = "0.0.0.0:4421";
+	info!("Starting server on '{address}'");
+	server.serve(address).await
 }
--- server/src/statbotics.rs
+++ server/src/statbotics.rs
@@ -1,11 +1,15 @@
+use std::collections::HashSet;
 use std::sync::Arc;
 use std::time::Instant;
 use std::{collections::HashMap, time::Duration};
 
+use color_eyre::Result;
+use log::{error, info};
 use poem_openapi::Object;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use tokio::sync::RwLock;
+use tokio::spawn;
+use tokio::sync::{Mutex, RwLock};
 use ts_rs::TS;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -38,14 +42,16 @@ pub struct StatboticsTeam {
 #[derive(Debug)]
 pub struct StatboticsCache {
 	client: Client,
-	teams: RwLock<HashMap<u32, Arc<StatboticsTeam>>>,
+	teams: Arc<RwLock<HashMap<u32, Arc<StatboticsTeam>>>>,
+	teams_loading: Arc<Mutex<HashSet<u32>>>,
 	year: u32,
 }
 
 impl StatboticsCache {
 	pub fn new(year: u32) -> Self {
 		Self {
-			teams: RwLock::new(HashMap::new()),
+			teams: Arc::new(RwLock::new(HashMap::new())),
+			teams_loading: Arc::new(Mutex::new(HashSet::new())),
 			client: Client::builder()
 				.user_agent(env!("CARGO_PKG_NAME"))
 				.build()
@@ -54,31 +60,67 @@ impl StatboticsCache {
 		}
 	}
 	pub async fn get(&self, team: u32) -> Option<Arc<StatboticsTeam>> {
-		if self
-			.teams
-			.read()
-			.await
-			.get(&team)
-			.map(|team| team.last_update.0.elapsed() > Duration::from_secs(5 * 60))
-			.unwrap_or(true)
-		{
-			self.teams.write().await.insert(
-				team,
-				Arc::new(
-					self.client
-						.get(format!(
-							"https://api.statbotics.io/v2/team_year/{team}/{}",
-							self.year
-						))
-						.send()
-						.await
-						.ok()?
-						.json::<StatboticsTeam>()
-						.await
-						.ok()?,
-				),
-			);
+		let team_stats = self.teams.read().await.get(&team).cloned();
+
+		match team_stats {
+			None => match Self::load_team(&self.client, self.year, team).await {
+				Ok(data) => {
+					info!("Statbotics ({team}): load complete");
+					self.teams.write().await.insert(team, data)
+				}
+				Err(err) => {
+					error!("Statbotics ({team}): load error: {err}");
+					None
+				}
+			},
+			Some(team_stats) => {
+				if team_stats.last_update.0.elapsed() > Duration::from_secs(5 * 60) {
+					self.trigger_load_team(team).await;
+				}
+
+				Some(team_stats)
+			}
 		}
-		Some(self.teams.read().await[&team].clone())
+	}
+	async fn load_team(client: &Client, year: u32, team: u32) -> Result<Arc<StatboticsTeam>> {
+		info!("Statbotics ({team}): Loading data");
+
+		Ok(Arc::new(
+			client
+				.get(format!(
+					"https://api.statbotics.io/v2/team_year/{team}/{}",
+					year
+				))
+				.send()
+				.await?
+				.json::<StatboticsTeam>()
+				.await?,
+		))
+	}
+	async fn trigger_load_team(&self, team: u32) {
+		let mut lock = self.teams_loading.lock().await;
+
+		if !lock.contains(&team) {
+			lock.insert(team);
+			drop(lock);
+
+			let client_clone = self.client.clone();
+			let teams_clone = self.teams.clone();
+			let teams_loading_clone = self.teams_loading.clone();
+			let year = self.year;
+			spawn(async move {
+				match Self::load_team(&client_clone, year, team).await {
+					Ok(data) => {
+						info!("Statbotics ({team}): background load complete");
+						teams_clone.write().await.insert(team, data);
+					}
+					Err(err) => {
+						error!("Statbotics ({team}): background load error: {err}");
+					}
+				}
+
+				teams_loading_clone.lock().await.remove(&team);
+			});
+		}
 	}
 }
