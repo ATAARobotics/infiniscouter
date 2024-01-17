cab0f98d1c07d66f63dc5cfd24e7b4e49bade586
Veda <veda@vedapowered.ca>
2024-01-16 19:37:47 -0700

    Start to implement pit screen

--- client/src/components/sync_button.tsx
+++ client/src/components/sync_button.tsx
@@ -7,14 +7,25 @@ import Divider from "@mui/joy/Divider";
 import ListItem from "@mui/joy/ListItem";
 import ListItemButton from "@mui/joy/ListItemButton";
 
+/**
+ *	A button that when clicked syncs important data from/to localStorage to/from the server
+ *	@returns the component
+ */
 export function SyncButton() {
   const [loadingState, setLoadingState] = React.useState<"saved" | "saving">(
     "saved",
   );
+  /**
+	* Sync data by loading event and game info from the server, sending local data, and fetching remote data.
+	*/
   async function doSync() {
     setLoadingState("saving");
-    const response = await fetch("/api/event/matches");
-    localStorage.setItem("matchList", await response.text());
+    const matchesStr = await fetch("/api/event/matches");
+    localStorage.setItem("matchList", await matchesStr.text());
+    const matchFieldsStr = await fetch("/api/match_entry/fields");
+    localStorage.setItem("matchFields", await matchFieldsStr.text());
+    const pitFieldsStr = await fetch("/api/pit_entry/fields");
+    localStorage.setItem("pitFields", await pitFieldsStr.text());
     const matchArray = [];
     for (let entry = 0; entry < localStorage.length; entry++) {
       const key: string | null = localStorage.key(entry);
--- client/src/pages/pit_entry.tsx
+++ client/src/pages/pit_entry.tsx
@@ -1,64 +1,155 @@
-import Button from "@mui/joy/Button";
-import Box from "@mui/joy/Box";
-import { AspectRatio, Card, CardContent, Typography } from "@mui/joy";
+import { useState, useEffect } from "preact/hooks";
+import { MatchEntryFields } from "../generated/MatchEntryFields";
+import { MatchEntryData } from "src/generated/MatchEntryData";
+import {
+  Box,
+  CircularProgress,
+  RadioGroup,
+  Radio,
+  Stack,
+  Typography,
+  Input,
+} from "@mui/joy";
+import { EventInfo } from "src/generated/EventInfo";
+import { MatchInfo } from "src/generated/MatchInfo";
+import { ChangeEvent } from "preact/compat";
+import { MatchEntryIdData } from "src/generated/MatchEntryIdData";
+import { MatchPage } from "src/components/entry_components";
+import { PitEntryFields } from "src/generated/PitEntryFields";
 
-// Pit Entry Page Component
+/**
+ * Pit Entry Page Component
+ * @returns the component
+ */
 export function PitEntry() {
+  const [matchId, setMatchId] = useState<number>();
+  const [teamId, setTeamId] = useState<number>();
+
+  const [data, setData] = useState<MatchEntryData>({ entries: {} });
+  useEffect(() => {
+    if (matchId !== undefined && teamId !== undefined) {
+      const saveData: MatchEntryIdData = {
+        match_id: matchId.toString(),
+        team_id: teamId.toString(),
+        data,
+      };
+      localStorage.setItem(
+        "match-" + matchId?.toString() + "-" + teamId?.toString(),
+        JSON.stringify(saveData),
+      );
+    }
+  }, [data]);
+  useEffect(() => {
+    if (matchId !== undefined && teamId !== undefined) {
+      const newData: MatchEntryIdData | null = JSON.parse(
+        localStorage.getItem(
+          "match-" + matchId?.toString() + "-" + teamId?.toString(),
+        ) ?? "null",
+      );
+      if (newData !== null) {
+        setData(newData.data);
+      } else {
+        setData({ entries: {} });
+      }
+    }
+  }, [matchId, teamId]);
+
+  const matchTeams: EventInfo | null = JSON.parse(
+    localStorage.getItem("matchList") ?? "null",
+  );
+  const fields: PitEntryFields | null = JSON.parse(
+    localStorage.getItem("matchList") ?? "null",
+  );
+  if (!matchTeams) {
+    return (
+      <Box>
+        <Typography>Click "Save Data" to get list of matches...</Typography>
+      </Box>
+    );
+  }
+  const teamsForMatch: MatchInfo | undefined | 0 =
+    matchId !== undefined
+      ? matchTeams.match_infos.filter(
+          (match) =>
+            match.id.match_type === "qualification" && match.id.num === matchId,
+        )[0]
+      : undefined;
+
   return (
     <Box>
-      <Card sx={{ width: 320 }}>
-        <div>
-          <Typography level="title-lg">4421 - Forge Robotics</Typography>
-          <Typography level="body-sm">Amount of Breaks: 0</Typography>
-        </div>
-        <AspectRatio minHeight="120px" maxHeight="200px">
-          <img src="https://i.ytimg.com/vi/pXpfVwNjV7Q/maxresdefault.jpg" />
-        </AspectRatio>
-        <CardContent orientation="horizontal">
-          <div>
-            <Typography level="body-sm">EPA:</Typography>
-            <Typography fontSize="lg" fontWeight="lg">
-              100
-            </Typography>
-          </div>
-          <Button
-            variant="solid"
-            size="md"
-            color="primary"
-            aria-label="View team"
-            sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
+      <h1>Match Entry Page</h1>
+      <Box>
+        <Input
+          type="number"
+          placeholder={"Qualification Match Number"}
+          onChange={(ev: InputEvent) => {
+            setMatchId(parseInt((ev.target as HTMLInputElement).value));
+            setTeamId(undefined);
+          }}
+        />
+      </Box>
+      {teamsForMatch && (
+        <Box>
+          <RadioGroup
+            onChange={(ev: ChangeEvent) =>
+              setTeamId(parseInt((ev.target as HTMLInputElement).value))
+            }
+            value={teamId ?? null}
           >
-            View
-          </Button>
-        </CardContent>
-      </Card>
-
-      <Card sx={{ width: 320 }}>
-        <div>
-          <Typography level="title-lg">4627 - Manning Robotics</Typography>
-          <Typography level="body-sm">Amount of Breaks: 0</Typography>
-        </div>
-        <AspectRatio minHeight="120px" maxHeight="200px">
-          <img src="https://hagadone.media.clients.ellingtoncms.com/ARTICLE_170429960_AR_0_WAMEMDNNOVJB_t1170.jpg?5cc718665ab672dba93d511ab4c682bb370e5f86" />
-        </AspectRatio>
-        <CardContent orientation="horizontal">
+            <Stack direction="row">
+              <Stack direction="column">
+                <Typography>RED</Typography>
+                {teamsForMatch?.teams_red.map((team) => (
+                  <Radio value={team} label={team.toString()} />
+                ))}
+              </Stack>
+              <Stack direction="column">
+                <Typography>BLUE</Typography>
+                {teamsForMatch?.teams_blue.map((team) => (
+                  <Radio value={team} label={team.toString()} />
+                ))}
+              </Stack>
+            </Stack>
+          </RadioGroup>
+        </Box>
+      )}
+      {teamsForMatch &&
+        teamId &&
+        (fields ? (
+          fields.pages.map((page) => (
+            <MatchPage
+              page={page}
+              entries={fields.entries}
+              setEntry={(id, value) => {
+                if (value === undefined) {
+                  const tmp = {
+                    entries: { ...data.entries },
+                    timestamp_ms: Date.now(),
+                  };
+                  delete tmp.entries[id];
+                  setData(tmp);
+                } else {
+                  setData({
+                    entries: { ...data.entries, [id]: value },
+                    timestamp_ms: Date.now(),
+                  });
+                }
+              }}
+              allEntries={data.entries}
+            ></MatchPage>
+          ))
+        ) : (
           <div>
-            <Typography level="body-sm">EPA:</Typography>
-            <Typography fontSize="lg" fontWeight="lg">
-              90
-            </Typography>
+            <CircularProgress
+              color="danger"
+              determinate={false}
+              size="sm"
+              value={25}
+              variant="solid"
+              thickness={7}
+            />
+            {"  "}Loading...
           </div>
-          <Button
-            variant="solid"
-            size="md"
-            color="primary"
-            aria-label="View team"
-            sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-          >
-            View
-          </Button>
-        </CardContent>
-      </Card>
+        ))}
     </Box>
-  );
 }
