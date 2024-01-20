30d405ead4081a65a1bff784fdbd9b0c0b3a5844
Jason Heard <jasonpheard@101100.ca>
2024-01-20 10:12:36 -0700

    Always load matches so they are available

--- client/src/components/sync_button.tsx
+++ client/src/components/sync_button.tsx
@@ -1,27 +1,44 @@
 import Button from "@mui/joy/Button";
-import * as React from "react";
 
 import { MatchEntryIdData } from "../generated/MatchEntryIdData";
+import { useEffect, useState } from "preact/hooks";
 
 /**
  *	A button that when clicked syncs important data from/to localStorage to/from the server
  *	@returns the component
  */
 export function SyncButton() {
-  const [loadingState, setLoadingState] = React.useState<"saved" | "saving">(
+  const [loadingState, setLoadingState] = useState<"saved" | "saving">(
     "saved",
   );
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetch("/api/event/matches", { signal: controller.signal })
+      .then(matchesResponse => matchesResponse.text())
+      .then(matchesStr => {
+        localStorage.setItem("matchList", matchesStr);
+      });
+    fetch("/api/match_entry/fields", { signal: controller.signal })
+      .then(matchesResponse => matchesResponse.text())
+      .then(matchesStr => {
+        localStorage.setItem("matchFields", matchesStr);
+      });
+    fetch("/api/pit_entry/fields", { signal: controller.signal })
+      .then(matchesResponse => matchesResponse.text())
+      .then(matchesStr => {
+        localStorage.setItem("pitFields", matchesStr);
+      });
+
+    return () => controller.abort();
+  }, []);
+
   /**
    * Sync data by loading event and game info from the server, sending local data, and fetching remote data.
    */
   async function doSync() {
     setLoadingState("saving");
-    const matchesStr = await fetch("/api/event/matches");
-    localStorage.setItem("matchList", await matchesStr.text());
-    const matchFieldsStr = await fetch("/api/match_entry/fields");
-    localStorage.setItem("matchFields", await matchFieldsStr.text());
-    const pitFieldsStr = await fetch("/api/pit_entry/fields");
-    localStorage.setItem("pitFields", await pitFieldsStr.text());
     const matchArray = [];
     for (let entry = 0; entry < localStorage.length; entry++) {
       const key: string | null = localStorage.key(entry);
