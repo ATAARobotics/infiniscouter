6d3de8da42302bc672df25df1e5aa44552f1090b
Jason Heard <jasonpheard@101100.ca>
2024-01-20 09:28:09 -0700

    Add signal-hook to ease stopping docker

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,17 +9,33 @@ mod server;
 mod statbotics;
 mod tba;
 
-use crate::config::ConfigManager;
-use crate::database::Database;
-use crate::server::ScoutingServer;
+use std::process;
+
 use color_eyre::Result;
 use log::info;
 use simplelog::{Config, LevelFilter, SimpleLogger};
+
+use crate::config::ConfigManager;
+use crate::database::Database;
+use crate::server::ScoutingServer;
 
 #[tokio::main]
 async fn main() -> Result<()> {
 	color_eyre::install()?;
 	SimpleLogger::init(LevelFilter::Info, Config::default())?;
+
+	// Shutdown on SIGINT or SIGTERM, or CTRL-C, for docker
+	#[cfg(not(target_os = "windows"))]
+	tokio::spawn(async {
+		use signal_hook::consts::{SIGINT, SIGTERM};
+		use signal_hook::iterator::Signals;
+
+		let mut signals = Signals::new([SIGINT, SIGTERM]).unwrap();
+		if signals.forever().next().is_some() {
+			info!("Shutting down due to SIGINT or SIGTERM");
+			process::exit(0);
+		}
+	});
 
 	let server = ScoutingServer::new(ConfigManager::new()?, Database::open("data")?)?;
 	let address = "0.0.0.0:4421";
