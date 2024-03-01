93917c460ece20a529ad6547594a51406197a47a
Veda <veda@vedapowered.ca>
2024-02-29 20:50:59 -0800

    Fix full image

--- client/src/pages/full_image.tsx
+++ client/src/pages/full_image.tsx
@@ -10,9 +10,9 @@ interface FullImageProps {
 export function FullImage(props: FullImageProps) {
 	return (
 		<div>
-			<Navbar title={""} />
+			<Navbar title={"Image"} />
 			<a href={`/image/full/${props.id}`} target="_blank">
-				<img src={`/image/full/${props.id}`} />
+				<img src={`/image/full/${props.id}`} style={{ transform: "rotate(90deg)", width: "90vw" }} />
 			</a>
 		</div>
 	);
